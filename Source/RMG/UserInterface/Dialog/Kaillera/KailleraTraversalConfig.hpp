1eb126329d536f95f0062fbe788fdc4269ffbcf0
nickthename <nickthename@users.noreply.github.com>
2026-03-28 18:00:08 -0400

    prod server

--- Source/RMG/UserInterface/Dialog/Kaillera/KailleraTraversalConfig.hpp
+++ Source/RMG/UserInterface/Dialog/Kaillera/KailleraTraversalConfig.hpp
@@ -1,7 +1,7 @@
 #ifndef KAILLERA_TRAVERSAL_CONFIG_HPP
 #define KAILLERA_TRAVERSAL_CONFIG_HPP
 
-static constexpr const char* kN02TraversalHost = "localhost";
+static constexpr const char* kN02TraversalHost = "nat.smash64.net";
 static constexpr int kN02TraversalPort = 6364;
 static constexpr const char* kN02TraversalProtocol = "N02TRAV1";
 
