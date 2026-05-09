c24d9fc10f9adcaf3139d987865e8e344c786f73
Nyx JPN <nyxtheshieldmusic@gmail.com>
2026-05-09 22:17:42 +0900

    Dont zero buffers

--- Source/3rdParty/mupen64plus-core/src/main/savestates.c
+++ Source/3rdParty/mupen64plus-core/src/main/savestates.c
@@ -1927,7 +1927,8 @@ static int savestates_save_m64p(const struct device* dev, char *filepath)
         return 0;
     }
 
-    memset(save->data, 0, allocation_size);
+    if (!rollback_buffer_save)
+        memset(save->data, 0, allocation_size);
 
     // Write the save state data to memory
     PUTARRAY(savestate_magic, curr, unsigned char, 8);
